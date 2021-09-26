a10b2a2daf3cdd55bf18b1b8d54817f821c2e57f
Earl Kwofie <earlmkwo@gmail.com>
2021-09-26 14:53:14 +0000

    Testing commit again

---
+++ HW1/.five_integers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	int inputs[5], results[3];
+
+	printf("Enter 5 integers");
+
+       	for(int i = 0; i < 5; i++)
+        {
+                scanf("%d", &inputs[i]);
+        }
+						
+}
